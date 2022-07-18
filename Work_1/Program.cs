88a86e2ebccb4330e2e0e0059a17029c155b63c8
Viktorinkiss <yurkovawika@gmail.com>
2022-07-18 21:41:13 +0500

    Задача 1.Промежуток чисел

---
+++ Work_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+int Prompt(string length) 
+{
+    System.Console.Write (length);
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintNumbers (int n,int m)
+{   System.Console.Write($" '(");
+    for (n = n ; n < m; n++)
+    {
+        System.Console.Write($"{n},");
+    }
+    System.Console.Write($"{n})'");
+}
+int n = Prompt("Введите первое число: ");
+int m = Prompt("Введите второе число: ");
+PrintNumbers(n,m);
