1b63fe4d70ee6dc3ec1aba6b70ab1954522b11be
Viktorinkiss <yurkovawika@gmail.com>
2022-07-18 21:44:29 +0500

    Задача 2. Сумма чисел от M до N

---
+++ Work_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+int Prompt(string length) 
+{
+    System.Console.Write (length);
+    return int.Parse(Console.ReadLine());
+}
+
+void SumNumbers(int n,int m)
+{   
+int sum = 0;
+    for (n = n ; n <= m;n++)
+    {
+        sum =sum + n;
+    }
+    System.Console.Write($"Сумма чисел = {sum}");
+}
+int n = Prompt("Введите первое число: ");
+int m = Prompt("Введите второе число: ");
+SumNumbers(n,m);
