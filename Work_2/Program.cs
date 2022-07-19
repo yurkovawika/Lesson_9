431e99082431134603ddec76afe11152de2dfd03
Viktorinkiss <yurkovawika@gmail.com>
2022-07-19 13:30:09 +0500

    Задача 2.Сумма чисел от N до M с помощью рекурсии

--- Work_2/Program.cs
+++ Work_2/Program.cs
@@ -1,21 +1,22 @@
-﻿//Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-
-
+﻿//Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от N до M с помощью рекурсии.
 int Prompt(string length) 
 {
     System.Console.Write (length);
     return int.Parse(Console.ReadLine());
 }
 
-void SumNumbers(int n,int m)
-{   
-int sum = 0;
-    for (n = n ; n <= m;n++)
+int SumNumbers(int n,int m,int sum)
+{  
+    if (n <= m)
     {
-        sum =sum + n;
+    sum = sum+n;
+    n++;
+    sum = SumNumbers(n,m,sum );
     }
-    System.Console.Write($"Сумма чисел = {sum}");
+    return sum;
 }
 int n = Prompt("Введите первое число: ");
 int m = Prompt("Введите второе число: ");
-SumNumbers(n,m);
+int sum = SumNumbers(n,m,0);
+System.Console.Write($"Сумма чисел от {n} + {m} = {sum}");
+
